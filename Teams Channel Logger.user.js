e2e5352bf1889199e48fc87655712be7b747d245
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2019-07-31 12:25:08 -0400

    Update Teams Channel Logger.user.js

--- Teams Channel Logger.user.js
+++ Teams Channel Logger.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Teams Channel Logger
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Scan and download the current teams channel log.
 // @author       Zachary Kitcher
 // @include      https://teams.microsoft.com*
@@ -22,38 +22,33 @@
             $('<a id="expandButton" style="cursor: pointer; font-size: 17px;">Expand All Entries</a>').insertAfter($('#settingsDropdown'));
 
             $('#expandButton').click(function() {
-                $('<a id="scanningEntries" style="font-size: 17px;">Scroll Up to Scan Entries. # of entries scanned: </a>').insertAfter($(this));
+                $('<a id="scanningEntries" style="font-size: 17px;">Scroll down to the latest entry</a>').insertAfter($(this));
                 $(this).hide();
 
                 log = "";
                 entry = "";
 
                 var entryCount = 0;
-                var headerCounter = 0;
-                var topCount = 0;
+				var headerCounter = 0;
+				var topCount = 0;
 
-                $('<a id="loadingIcon" style="font-size: 17px;">.</a>').insertAfter('#scanningEntries');
-                var loadingInterval = setInterval(function() {
-                    if(loading < 3){
-                        loading++;
-                    }else{
-                        loading = 1;
-                    };
-                    if(loading == 3){
-                        $('a[id="loadingIcon"]').text(entryCount+"...");
-                    }else if(loading == 2){
-                        $('a[id="loadingIcon"]').text(entryCount+".. ");
-                    }else{
-                        $('a[id="loadingIcon"]').text(entryCount+".  ");
-                    };
-                }, 500);
-
-                var expantInterval = setInterval(function() {
-					if(entryCount == 0){
-						$('a[id="scanningEntries"]').text("Scroll down to the latest entry. # of entries scanned: ");
+				$('<a id="loadingIcon" style="font-size: 17px;">.</a>').insertAfter('#scanningEntries');
+				var loadingInterval = setInterval(function() {
+					if(loading < 3){
+						loading++;
 					}else{
-						$('a[id="scanningEntries"]').text("Scroll Up to Scan Entries. # of entries scanned: ");
+						loading = 1;
+                    };
+					if(loading == 3){
+						$('a[id="loadingIcon"]').text("...");
+					}else if(loading == 2){
+						$('a[id="loadingIcon"]').text(".. ");
+					}else{
+						$('a[id="loadingIcon"]').text(".  ");
 					};
+				}, 500);
+
+				var expantInterval = setInterval(function() {
 					if ($('a[id="scanningEntries"]').length === 0){
 						clearInterval(expantInterval);
 						log = "";
@@ -61,46 +56,48 @@
 						console.log("Scan Interrupted");
 					};
 
-                    //Expand all rolled up threads
-                    $('div[data-tid="threadViewToggle"]').each(function(){
-                        if ($(this).attr("title").includes("Collapse all") == false){
-                            $(this).click();
-                        };
-                    });
+					//Expand all rolled up threads
+					$('div[data-tid="threadViewToggle"]').each(function(){
+						if ($(this).attr("title").includes("Collapse all") == false){
+							$(this).click();
+						};
+					});
 
-                    $('div[data-scroll-pos]').each(function(){
-                        title = ("<h1>" + $('h2[class="ts-title team-name"]').text() + "</h1><br>");
+					$('div[data-scroll-pos]').each(function(){
+						title = ("<h1>" + $('h2[class="ts-title team-name"]').text() + "</h1><br>");
 
-                        if($(this).attr('data-scroll-pos') == entryCount){
+						if($(this).attr('data-scroll-pos') == entryCount){
+							var systemEntryCheck = $(this).find('system-message[data-tid]').attr('data-tid');
+							var deletedMessageCheck = $(this).find('div[id*="deleted-message"]').attr('id')
+							if(systemEntryCheck == null && deletedMessageCheck == null){
+								console.log(entryCount);
+								$(this).find('div[scroll-item-id]').each(function(){
+									if($(this).attr("scroll-item-id") != null){
+										var firstEntryCheck = $(this).attr("scroll-item-id") //First Message
+										var agentName = $(this).find('div[data-tid="threadBodyDisplayName"]').html().replace(/\s\B/g, '') //Name of Agent
+										var entryTimeSent = $(this).find('span[data-tid="messageTimeStamp"]').html().replace(/\s\B/g, '') //Time sent
+										var entryBody = $(this).find('div[data-tid="messageBodyContent"]').html().replace(/#/g, '-') //Entry Body
 
-                            var systemEntryCheck = $(this).find('system-message[data-tid]').attr('data-tid');
-                            var deletedMessageCheck = $(this).find('div[id*="deleted-message"]').attr('id')
-                            if(systemEntryCheck == null && deletedMessageCheck == null){
-                                console.log(entryCount);
-                                $(this).find('div[scroll-item-id]').each(function(){
-                                    if($(this).attr("scroll-item-id") != null){
-                                        var firstEntryCheck = $(this).attr("scroll-item-id") //First Message
-                                        var agentName = $(this).find('div[data-tid="threadBodyDisplayName"]').html().replace(/\s\B/g, '') //Name of Agent
-                                        var entryTimeSent = $(this).find('span[data-tid="messageTimeStamp"]').html().replace(/\s\B/g, '') //Time sent
-                                        var entryBody = $(this).find('div[data-tid="messageBodyContent"]').html().replace(/#/g, '-') //Entry Body
-
-                                        if (firstEntryCheck.includes("firstMessage") == true){
-                                            log = "<br>"+ entry +"<br>"+ log;
-                                            entry = "";
-                                            entry = "<b>"+ agentName +"</b>"+ entryTimeSent+"<br>" +entryBody + entry;
-                                        }else{
-                                            entry = entry +"<p><b>"+ agentName +"</b>"+ entryTimeSent +"<br>"+ entryBody.replace(/<div>/g, '').replace(/<div/g, '<p')+ "</p>";
-                                        };
-                                    };
-                                });
-                                entryCount++;
-                            }else{
-                                console.log("System Message");
-                                entryCount++;
-                            };
-                        };
-                    });
-                    var topOfPageCheck = $('div[class*="list-wrap list-wrap-v3 ts-message-list-container"]').position('top').top;
+										if (firstEntryCheck.includes("firstMessage") == true){
+											log = "<br>"+ entry +"<br>"+ log;
+											entry = "";
+											entry = "<b>"+ agentName +"</b>"+ entryTimeSent+"<br>" +entryBody + entry;
+										}else{
+											entry = entry +"<p><b>"+ agentName +"</b>"+ entryTimeSent +"<br>"+ entryBody.replace(/<div>/g, '').replace(/<div/g, '<p')+ "</p>";
+										};
+									};
+								});
+								if(entryCount != 0){
+									$('a[id="scanningEntries"]').text("Scroll Up to Scan Entries. # of entries scanned: " + entryCount);
+								};
+								entryCount++;
+							}else{
+								console.log("System Message");
+								entryCount++;
+							};
+						};
+					});
+					var topOfPageCheck = $('div[class*="list-wrap list-wrap-v3 ts-message-list-container"]').position('top').top;
                     if(topOfPageCheck == 0 && entryCount != 0){
 						$('a[id="scanningEntries"]').text("Formatting Download");
                         topCount++;
